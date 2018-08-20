b6b6c80d91dcee94e29571ab6ad465827b3cffdc
Vincent Lemeunier <vincent.lemeunier@algolia.com>
2018-08-20 13:10:16 +0200

    WIP

--- preview/index.js
+++ preview/index.js
@@ -1,16 +1,21 @@
-import React from 'react';
-import { render } from 'react-dom';
-import BaselineAnnouncement from '../dist/BaselineAnnouncement.js';
+import React from "react";
+import { render } from "react-dom";
+import * as mdIcons from "../dist/index.js";
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <h1>react-md-icon</h1>
-        <BaselineAnnouncement style={{ fontSize: '10em', color: 'purple' }} />
+        <div id="icon-container">
+          {Object.keys(mdIcons).map(iconName => {
+            const Icon = mdIcons[iconName];
+            return <Icon id={iconName} key={iconName} />;
+          })}
+        </div>
       </div>
     );
   }
 }
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
---
+++ preview/webpack.config.js
@@ -0,0 +1,21 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./preview/index.html",
+  filename: "./index.html"
+});
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: [htmlPlugin]
+};
